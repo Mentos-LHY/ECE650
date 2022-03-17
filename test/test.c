85e4a4138ad6b5d32259a7ac078f3dae58a247e4
Mentos-LHY <larry980822@gmail.com>
2022-03-17 17:39:28 -0400

    hw4 finish

---
+++ test/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void function(int a, int b, int c) {
+  char buffer1[5];
+  char buffer2[20];
+  //////////////////////////////////////////////////////////////////
+  int * ret;
+  ret = (void *)(buffer1 + 21);
+  (*ret) += 7;
+  //////////////////////////////////////////////////////////////////
+}
+
+void main() {
+  int x;
+
+  x = 0;
+  function(1, 2, 3);
+  x = 1;
+  printf("x=%d\n", x);
+}
