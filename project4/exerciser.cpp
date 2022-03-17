3c5b26ebb79db100c1aca40f560807a5c89d7149
Mentos-LHY <larry980822@gmail.com>
2022-03-17 18:23:12 -0400

    hw4 final

--- project4/exerciser.cpp
+++ project4/exerciser.cpp
@@ -1,5 +1,6 @@
 #include "exerciser.h"
 
 void exercise(connection * C) {
-  query1(C, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  query1(C, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0.5, 1.0);
+  query4(C, "NC", "DarkBlue");
 }
